27bc2e94f1886a8418ce93ece63a6cb6982b4e83
kkas18 <kasim18@yahoo.no>
2026-09-02 08:37:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    men et nytt versjonsnummer garanterer at gammel cache ryddes.)
    ================================================================ */
 
-const VERSION = '2.0.1';
+const VERSION = '2.1.0';
 const CACHE   = 'numera-' + VERSION;
 
 const PRECACHE = [
